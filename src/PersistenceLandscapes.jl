ce5274b8fcce1985b78712f69d80fdb7688c9440
edd26 <e.dmitruk@herts.ac.uk>
2021-02-22 11:31:29 +0100

    Added export for a few new functions

--- src/PersistenceLandscapes.jl
+++ src/PersistenceLandscapes.jl
@@ -1,5 +1,20 @@
 module PersistenceLandscapes
 
+    export MyPair,
+            PersistenceBarcodes,
+            size,
+            isempty,
+            copy,
+            dim,
+            compareAccordingToLength,
+            removeBarcodesThatBeginsBeforeGivenNumber,
+            putToBins,
+            compareMyPairs,
+            sort,
+            compare,
+            minMax
+
+    include("PersistenceBarcode.jl")
 # Write your package code here.
 
 end
