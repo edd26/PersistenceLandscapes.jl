2bd250eba49db19204536f0135b5c1fb85b64b62
edd26 <e.dmitruk@herts.ac.uk>
2021-03-02 17:15:53 +0100

    Changed copyrights, add ArgParse

--- src/ComputeAverage.jl
+++ src/ComputeAverage.jl
@@ -1,5 +1,5 @@
-#    Copyright 2013-2014 University of Pennsylvania
-#    Created by Pawel Dlotko
+#    Copyright 2021
+#    Translation of Pawel Dlotko C++ library by Emil Dmitruk
 #
 #    This file is part of Persistence Landscape Toolbox (PLT).
 #
@@ -9,17 +9,14 @@
 #    (at your option) any later version.
 #
 #    PLT is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    but WITHOUT ANY WARRANTY without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU Lesser General Public License for more details.
 #
 #    You should have received a copy of the GNU Lesser General Public License
 #    along with PLT.  If not, see <http://www.gnu.org/licenses/>.
 
-
-
-
-
+using ArgParse
 
 include("FilesReader.jl")
 include("VectorSpaceOfPersistenceLandscapes.jl")
@@ -30,54 +27,50 @@ A file containing a list of files with persistence landscapes (.lan files) or pe
 """
 
 
-int main(int argc, char *argv[])
-{
-    configure();
+function main(int argc, char *argv[])
+
+    configure()
     println(programInfo)
 
     if ( argc != 2 )
-    {
-        println(endl)
         println("Wrong usage of a program. Please call <program name> ")
         println("<name of the file with names of files with persistence landscapes/barcodes> ")
         println("The program will now terminate.")
-        return 1;
-    }
+        return 1
+    end
 
-    char* fileWithInputFiles = argv[1];
+    fileWithInputFiles = argv[1]
 
     println("Here are the parameters of the program: ")
     println("Name of the file with barcodes \\ landscapes : $(2)")
 
     #End of a small talk with the User. Let's get to work.
     #First, read the names of files:
-    vector<string> namesOfFiles = readFileNames( fileWithInputFiles );
+    namesOfFiles = readFileNames( fileWithInputFiles )
 
     #now generate landscapes based on that files:
     println("Creating Persistence Landscapes.")
-    vector< PersistenceLandscape > landscapes = createLandscapesFromTheFiles( namesOfFiles );
+    landscapes = createLandscapesFromTheFiles( namesOfFiles )
     println("Done.")
 
     #And finally, do the permutation test:
     println("Computing avereage landscape. When finished, they will be found in the file 'distances.txt'")
 
-    vectorSpaceOfPersistenceLandscapes v;
-    for ( size_t i = 0 ; i != landscapes.size() ; ++i )
-    {
-        v.addLandscape( landscapes[i] );
-    }
-
-    PersistenceLandscape average = v.average();
+    v = vectorSpaceOfPersistenceLandscapes()
+    for  size_t i = 0 : size(landscapes)
+        v.addLandscape( landscapes[i] )
+    end
+    PersistenceLandscape average = v.average()
 
 
-    ostringstream name;
-    name << fileWithInputFiles << "_average.lan";
-    string fName = name.str();
-    const char* FName = fName.c_str();
-    average.printToFile( (char*)FName );
+    ostringstream name
+    name << fileWithInputFiles << "_average.lan"
+    string fName = name.str()
+    FName = fName.c_str()
+    average.printToFile( (char*)FName )
 
     println("That is all. Have a good day!")
 
 
-	return 0;
-}
+	return 0
+end
