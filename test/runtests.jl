8e66df99ce639fb3b80dddcad6427b9418b8eb17
edd26 <e.dmitruk@herts.ac.uk>
2021-07-12 13:53:55 +0200

    Changed test description; removed empty tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,27 +3,27 @@ using PersistenceLandscapes
 
 include("tests_configuration.jl")
 
-@testset "PersistenceBarcode tests" begin
+@testset "Test construction of PersistenceBarcode" begin
     include("persistenceBarcode_tests.jl")
 end
 
 
-@testset "PersistenceLandscape tests" begin
+@testset "Test contruction of PersistenceLandscape " begin
     include("persistenceLandscape_test.jl")
 end
 
-@testset "Configure tests" begin
-    include("configure_test.jl")
-end
+# @testset "Configure tests" begin
+#     include("configure_test.jl")
+# end
 
-@testset "Anova tests" begin
-    include("anova_test.jl")
-end
+# @testset "Anova tests" begin
+#     include("anova_test.jl")
+# end
 
-@testset "Main tests" begin
-    include("main_test.jl")
-end
+# @testset "Main tests" begin
+#     include("main_test.jl")
+# end
 
-@testset "VectorSpaceOfPersistenceLandscapes test" begin
+@testset "Test contruction of VectorSpaceOfPersistenceLandscapes" begin
     include("vectorSpaceOfPersistenceLandscapes_test.jl")
 end
