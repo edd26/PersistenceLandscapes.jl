224799193853adbb61d1222fb29871f73487bac1
edd26 <e.dmitruk@herts.ac.uk>
2021-09-29 19:52:06 +0200

    Visual refactoring, added comparison tests

---
+++ test/results_comparison_test.jl
@@ -0,0 +1,5 @@
+
+@testset "Compare results with original code" begin
+    # Average landscape computed for a single barcode
+
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,29 +1,41 @@
 using Test
 using PersistenceLandscapes
 
+# include("test/tests_configuration.jl")
 include("tests_configuration.jl")
 
+## ===-
 @testset "Test construction of PersistenceBarcode" begin
     include("persistenceBarcode_tests.jl")
 end
 
 
+## ===-
 @testset "Test contruction of PersistenceLandscape " begin
     include("persistenceLandscape_test.jl")
 end
 
+## ===-
 # @testset "Configure tests" begin
 #     include("configure_test.jl")
 # end
 
+## ===-
 # @testset "Anova tests" begin
 #     include("anova_test.jl")
 # end
 
+## ===-
 # @testset "Main tests" begin
 #     include("main_test.jl")
 # end
 
+## ===-
 @testset "Test contruction of VectorSpaceOfPersistenceLandscapes" begin
     include("vectorSpaceOfPersistenceLandscapes_test.jl")
+end
+
+## ===-
+@testset "Compare results with results from C++ version of PLT" begin
+    include("results_comparison_test.jl")
 end
