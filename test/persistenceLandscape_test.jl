50717e9374052af8daebaeb373193ec78a5d4e58
ed19aaf <e.dmitruk@herts.ac.uk>
2022-08-25 13:33:40 +0100

    Added tests on pl addition; refactored pl generation

--- test/persistenceLandscape_test.jl
+++ test/persistenceLandscape_test.jl
@@ -85,68 +85,162 @@ end
 
 ## ===-
 @testset "PersistenceLandscape operations" begin
-    ##
-    pl0, pl1, pl2, pl3 = map(
+    pl0, pl1, pl2, pl3, pl4, pl5, pl6, pl7, pl8, pl9 = map(
         x -> x |> PersistenceBarcodes |> PersistenceLandscape,
         [
             [MyPair(1, 3)],
             [MyPair(0, 4)],
             [MyPair(0, 2)],
             [MyPair(2, 4)],
+            [MyPair(0,4), MyPair(5,7)] ,
+            [MyPair(0,2), MyPair(2,4)] ,
+            [MyPair(1,3), MyPair(0,4)] ,
+            [MyPair(0,2), MyPair(0,4)] ,
+            [MyPair(2,6), MyPair(2,4), MyPair(4,6)] ,
+            [MyPair(0,6), MyPair(1,7), MyPair(4,8), MyPair(3,5)] ,
         ],
     )
-    pl0 = PersistenceLandscape(PersistenceBarcodes([MyPair(1,3)],1))
-    pl1 = PersistenceLandscape(PersistenceBarcodes([MyPair(0,4)],1))
-    pl2 = PersistenceLandscape(PersistenceBarcodes([MyPair(0,2)],1))
-    pl3 = PersistenceLandscape(PersistenceBarcodes([MyPair(2,4)],1))
-    ##
     @testset "equality" begin
         @test typeof(pl0+pl0) == PersistenceLandscape
         @test typeof(pl0-pl0) == PersistenceLandscape
         @test typeof(pl0*1) == PersistenceLandscape
 
-        @test (pl0*1).land[1][1] == MyPair(1,2)
-        @test (pl0*1).land[2][1] == MyPair(2,6)
-        @test (pl0*2).land[1][1] == MyPair(1,4)
-        @test (pl0*2).land[2][1] == MyPair(2,12)
+        @test (pl0*1).land[1][1] == MyPair(1,0)
+        @test (pl0*1).land[1][2] == MyPair(2,1)
+        @test (pl0*2).land[1][1] == MyPair(1,0)
+        @test (pl0*2).land[1][2] == MyPair(2,2)
 
         @test (pl1==pl1) == true
         @test (pl1==pl2) == false
         @test (pl0==pl1) == false
     end
 
-    ##
     @testset "addition" begin
-
-        # Check adding single element
-
+        # ===-
         # comutative test
         @test pl0 + pl0 == pl0 + pl0
         @test pl0 + pl1 == pl1 + pl0
         @test pl0 + pl2 == pl2 + pl0
+        @test pl0 + pl4 == pl4 + pl0
+        @test pl0 + pl5 == pl5 + pl0
         @test pl2 + pl3 == pl3 + pl2
 
-
+        # ===-
+        # test results correctness
         @test pl0 + pl0 == 2*pl0
-        @test pl0 + pl1 == PersistenceLandscape([[
-                                                                                    MyPair(0,0),
-                                                                                    MyPair(1,1),
-                                                                                    MyPair(2,3),
-                                                                                    MyPair(3,1),
-                                                                                    MyPair(4,0)
-                                                                                   ]],1)
-        @test pl0 + pl2 == PersistenceLandscape([[MyPair(0, 0),
-                                                                                    MyPair(1, 1),
-                                                                                    MyPair(2, 1),
-                                                                                    MyPair(3, 0)
-                                                                                   ]],1)
-        @test pl2 + pl3 == PersistenceLandscape([[MyPair(0, 0),
-                                                                                    MyPair(1, 1),
-                                                                                    MyPair(2, 0),
-                                                                                    MyPair(3, 1),
-                                                                                    MyPair(4, 0)
-                                                                                   ]], 1)
+        @test pl9 + pl9 == 2*pl9
+        @test pl0 + pl1 == PersistenceLandscape(
+            [[MyPair(0, 0), MyPair(1, 1), MyPair(2, 3), MyPair(3, 1), MyPair(4, 0)]],
+            1,
+        )
+        @test pl0 + pl2 ==
+        [[MyPair(0, 0), MyPair(1, 1), MyPair(2, 1), MyPair(3, 0)]] |> PersistenceLandscape
+        @test pl2 + pl3 ==
+        [[MyPair(0, 0), MyPair(1, 1), MyPair(2, 0), MyPair(3, 1), MyPair(4, 0)]] |>
+              PersistenceLandscape
+        @test pl2 + pl5 ==
+              [[MyPair(0, 0), MyPair(1, 2), MyPair(2, 0), MyPair(3, 1), MyPair(4, 0)]] |>
+              PersistenceLandscape
 
+
+        # ===-
+        # Landscapes with diferent number of layers
+        @test length((pl6 + pl0).land) == max(length(pl6.land), length(pl0.land))
+        @test (pl6 + pl0) ==
+        [[MyPair(0, 0), MyPair(1, 1), MyPair(2, 3),MyPair(3, 1), MyPair(4, 0)],[MyPair(1, 0), MyPair(2, 1), MyPair(3, 0)]] |>
+              PersistenceLandscape
+
+        @test length((pl6 + pl2).land) == max(length(pl6.land), length(pl2.land))
+        @test (pl6 + pl2) ==
+              [
+            [MyPair(0, 0), MyPair(1, 2), MyPair(2, 2), MyPair(4, 0)],
+            [MyPair(1, 0), MyPair(2, 1), MyPair(3, 0)],
+        ] |> PersistenceLandscape
+
+        @test length((pl7 + pl0).land) == max(length(pl7.land), length(pl2.land))
+        @test (pl7 + pl0) ==
+              [
+            [MyPair(0, 0), MyPair(1, 1), MyPair(2, 3), MyPair(3, 1),MyPair(4, 0)],
+            [MyPair(0, 0), MyPair(1, 1), MyPair(2, 0)],
+        ] |> PersistenceLandscape
+
+        @test length((pl7 + pl2).land) == max(length(pl7.land), length(pl2.land))
+        @test (pl7 + pl2) ==
+              [
+            [MyPair(0, 0), MyPair(1, 2), MyPair(2, 2), MyPair(4, 0)],
+            [MyPair(0, 0), MyPair(1, 1), MyPair(2, 0)],
+        ] |> PersistenceLandscape
+
+        @test length((pl6 + pl7).land) == max(length(pl6.land), length(pl7.land))
+        @test (pl6 + pl7) ==
+              [
+            [MyPair(0, 0), MyPair(1, 2), MyPair(2, 4), MyPair(4, 0)],
+            [MyPair(0, 0), MyPair(1, 1), MyPair(2, 1), MyPair(3,0)],
+        ] |> PersistenceLandscape
+
+        @test length((pl7 + pl8).land) == max(length(pl7.land), length(pl8.land))
+        @test (pl7 + pl8) ==
+              [
+            [MyPair(0, 0), MyPair(1, 1), MyPair(2, 2), MyPair(3, 2), MyPair(4, 2), MyPair(6, 0)],
+            [
+                MyPair(0, 0),
+                MyPair(1, 1),
+                MyPair(2, 0),
+                MyPair(3, 1),
+                MyPair(4, 0),
+                MyPair(5, 1),
+                MyPair(6, 0),
+            ],
+        ] |> PersistenceLandscape
+
+        @test length((pl0 + pl9).land) == max(length(pl0.land), length(pl9.land))
+        @test (pl0 + pl9) ==
+              [
+            [
+                MyPair(0, 0),
+                MyPair(1, 1),
+                MyPair(2, 3),
+                MyPair(3, 3),
+                MyPair(3.5, 2.5),
+                MyPair(4, 3),
+                MyPair(5.5, 1.5),
+                MyPair(6, 2),
+                MyPair(8, 0),
+            ],
+            [MyPair(1, 0), MyPair(3.5, 2.5), MyPair(5, 1), MyPair(5.5, 1.5), MyPair(7, 0)],
+            [MyPair(3, 0), MyPair(4, 1), MyPair(4.5, 0.5), MyPair(5, 1), MyPair(6, 0)],
+            [MyPair(4, 0), MyPair(4.5, 0.5), MyPair(5, 0)],
+        ] |> PersistenceLandscape
+
+        @test length((pl8 + pl9).land) == max(length(pl8.land), length(pl9.land))
+        @test (pl8 + pl9) ==
+              [
+            [
+                MyPair(0, 0),
+                MyPair(2, 2),
+                MyPair(3, 4),
+                MyPair(3.5, 4),
+                MyPair(4, 5),
+                MyPair(5.5, 2),
+                MyPair(6, 2),
+                MyPair(8, 0),
+            ],
+            [
+                MyPair(1, 0),
+                MyPair(2, 1),
+                MyPair(3, 3),
+                MyPair(3.5, 3),
+                MyPair(4, 2),
+                MyPair(5, 2),
+                MyPair(5.5, 2),
+                MyPair(6, 1),
+                MyPair(7, 0),
+            ],
+            [MyPair(3, 0), MyPair(4, 1), MyPair(4.5, 0.5), MyPair(5, 1), MyPair(6, 0)],
+            [MyPair(4, 0), MyPair(4.5, 0.5), MyPair(5, 0)],
+        ] |> PersistenceLandscape
+
+        # ===-
         # first values innew landscape should be a unique colleciton of x vals from both components
         for land1 = [pl0, pl1, pl2, pl3]
             for land2 = [pl0, pl1, pl2, pl3]
@@ -179,7 +273,67 @@ end
         # plot!(plt_average , ticks=0:1:10, xlims=[1,11])
         # final_plot = plot(plt_a, plt_b, plt_average, layout=(3,1), size=(600, 400*3))
     end
+## ==-===-
+@testset "substraction test" begin
+    pl0 = PersistenceLandscape(PersistenceBarcodes([MyPair(1,3)],1))
+    pl1 = PersistenceLandscape(PersistenceBarcodes([MyPair(0,4)],1))
+    pl2 = PersistenceLandscape(PersistenceBarcodes([MyPair(0,2)],1))
+    pl3 = PersistenceLandscape(PersistenceBarcodes([MyPair(2,4)],1))
+
+    # zero landscapes tests
+    diff_result = pl0 - pl0
+    for land_point in diff_result.land[1]
+        @test land_point.second == 0
+    end
+
+    diff_result = pl1 - pl1
+    for land_point in diff_result.land[1]
+        @test land_point.second == 0
+    end
+
+    diff_result = pl2 - pl2
+    for land_point in diff_result.land[1]
+        @test land_point.second == 0
+    end
+
+    # Other tests
+    diff_result = pl1 - pl2
+    @test diff_result.land[1][1].second == 0
+    @test diff_result.land[1][2].second == 0
+    @test diff_result.land[1][3].second == 2
+    @test diff_result.land[1][4].second == 0
+
+    # Tests for layered landscapes
+    two_layer_landscape_a = PersistenceLandscape(PersistenceBarcodes([MyPair(0,4), MyPair(0,2)],1))
+    two_layer_landscape_b = PersistenceLandscape(PersistenceBarcodes([MyPair(0,4), MyPair(1,3)],1))
+    two_layer_landscape_c = PersistenceLandscape(PersistenceBarcodes([MyPair(0,4), MyPair(2,4)],1))
+    two_layer_landscape_d = PersistenceLandscape(PersistenceBarcodes([MyPair(0,4), MyPair(3,5)],1))
+
+    diff_result = two_layer_landscape_a - two_layer_landscape_a
+    for k in 1:2
+        for land_point in diff_result.land[k]
+            @test land_point.second == 0
+        end
+    end
+
+    diff_result = two_layer_landscape_b - two_layer_landscape_b
+    for k in 1:2
+        for land_point in diff_result.land[k]
+            @test land_point.second == 0
+        end
+    end
+
+    diff_result1 = two_layer_landscape_a - two_layer_landscape_b
+    diff_result2 = two_layer_landscape_b - two_layer_landscape_a
+    @test diff_result1.land[1] == diff_result2.land[1]
+    @test diff_result1.land[2][1] == diff_result2.land[2][1]
+    @test abs(diff_result1.land[2][2].second) == abs(diff_result2.land[2][2].second)
+    @test abs(diff_result1.land[2][3].second) == abs(diff_result2.land[2][3].second)
+    @test diff_result1.land[2][4] == diff_result2.land[2][4]
+
 end
+end
+##
 
 ## ===-
 @testset "Distances tests" begin
@@ -194,113 +348,10 @@ end
 end
 
 
-## ===-
-@testset "PersistenceLandscape test on eirene results" begin
-# Reproduction of figure 4 from Bernadete paper about average landscape
-    x1 = 10
-    x2 = x1+1
-    x3 = x2+1
-    x4 = x3+1
-    x5 = x4+1
-    x6 = x5+1
-    hexa_matrix = [ 0  1  8  7  9  6;
-                    1  0  2 x1 x2 x3;
-                    8  2  0  3 x4 x5;
-                    7 x1  3  0  4 x6;
-                    9 x2 x4  4  0  5 ;
-                    6 x3 x5 x6  5  0;
-                    ]
-
-    x = collect(1:45) .+ 6
-    x = zeros(Int, 45) .+7
-    #                   a     b     c     d     e     f     g     h     i     j     k     l;
-    fig4_matrix = [     0     1     3     5  x[1]  x[2]     3     5     4     7     5     1; # a
-                        1     0     1  x[6]  x[7]  x[8]     6  x[9] x[10] x[11] x[12] x[13]; # b
-                        3     1     0     1     3 x[14]     5 x[15] x[16] x[17] x[18] x[19]; # c
-                        5  x[6]     1     0     1 x[20]     5 x[20] x[21] x[22] x[23] x[24]; # d
-                     x[1]  x[7]     3     1     0     1     3 x[25] x[26] x[27] x[28] x[29]; # e
-                     x[2]  x[8] x[14] x[20]     1     0     2 x[30] x[31] x[32] x[33] x[34]; # f
-                        3     6     5     5     3     2     0     1 x[35] x[36] x[37] x[38]; # g
-                        5  x[9] x[15] x[20] x[25] x[30]     1     0     1 x[39] x[40] x[41]; # h
-                        4 x[10] x[16] x[21] x[26] x[31] x[35]     1     0     2 x[42] x[43]; # i
-                        7 x[11] x[17] x[22] x[27] x[32] x[36] x[39]     2     0     1 x[44]; # j
-                        5 x[12] x[18] x[23] x[28] x[33] x[37] x[40] x[42]     1     0     1; # k
-                        1 x[13] x[19] x[24] x[29] x[34] x[38] x[41] x[43] x[44]     1     0; # l
-                 ]
-
-
-    x = collect(1:45) .+ 7
-    #                   a     b     c     d     e     f     g     h     i     j     k     l;
-    fig4_matrix_modified= [
-                        0     1     3     6  x[1]  x[2]     3     5     4     8     5     1; # a
-                        1     0     1  x[6]  x[7]  x[8]     7  x[9] x[10] x[11] x[12] x[13]; # b
-                        3     1     0     1     3 x[14]     6 x[15] x[16] x[17] x[18] x[19]; # c
-                        6  x[6]     1     0     1 x[20]     6 x[20] x[21] x[22] x[23] x[24]; # d
-                     x[1]  x[7]     3     1     0     1     3 x[25] x[26] x[27] x[28] x[29]; # e
-                     x[2]  x[8] x[14] x[20]     1     0     2 x[30] x[31] x[32] x[33] x[34]; # f
-                        3     7     6     6     3     2     0     1 x[35] x[36] x[37] x[38]; # g
-                        5  x[9] x[15] x[20] x[25] x[30]     1     0     1 x[39] x[40] x[41]; # h
-                        4 x[10] x[16] x[21] x[26] x[31] x[35]     1     0     2 x[42] x[43]; # i
-                        8  x[11] x[17] x[22] x[27] x[32] x[36] x[39]     2     0     1 x[44]; # j
-                        5 x[12] x[18] x[23] x[28] x[33] x[37] x[40] x[42]     1     0     1; # k
-                        1 x[13] x[19] x[24] x[29] x[34] x[38] x[41] x[43] x[44]     1     0; # l
-                 ]
-
-
-    x = collect(1:45) .+ 22
-    #                   a     b     c     d     e     f     g     h     i     j     k     l;
-    fig4_matrix_modified2 = [
-                        0     1     13   18  x[2]  x[3]    14    19    17  x[1]    20    10; # a
-                        1     0     2  x[6]  x[7]  x[8]    23  x[9] x[10] x[11] x[12] x[13]; # b
-                       13     2     0     3    15 x[14]    21 x[15] x[16] x[17] x[18] x[19]; # c
-                       18  x[6]     3     0     4  x[20]   22 x[20] x[21] x[22] x[23] x[24]; # d
-                     x[2]  x[7]    15     4     0     5    16 x[25] x[26] x[27] x[28] x[29]; # e
-                     x[3]  x[8] x[14] x[20]     5     0    11 x[30] x[31] x[32] x[33] x[34]; # f
-                       14    23    21    22    16    11     0     6 x[35] x[36] x[37] x[38]; # g
-                       19  x[9] x[15] x[20] x[25] x[30]     6     0     7 x[39] x[40] x[41]; # h
-                       17 x[10] x[16] x[21] x[26] x[31] x[35]     7     0    12 x[42] x[43]; # i
-                     x[1] x[11] x[17] x[22] x[27] x[32] x[36] x[39]    12     0     8 x[44]; # j
-                       20 x[12] x[18] x[23] x[28] x[33] x[37] x[40] x[42]     8     0     9; # k
-                       10 x[13] x[19] x[24] x[29] x[34] x[38] x[41] x[43] x[44]     9     0; # l
-                 ]
-
-    # Eirene computations
-
-    # ===-
-    vertices_labels = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
-                        "m", "n", "o"]
-
-    ord_matrices = Dict(:simpler_case => hexa_matrix,
-                         :fig4 => fig4_matrix,
-                         :fig4_modified => fig4_matrix_modified,
-                         :fig4_modified2 => fig4_matrix_modified2,
-                         )
-    all_keys = [:simpler_case, :fig4, :fig4_modified, :fig4_modified2]
-    C_ord_mat = Dict()
-
-    max_B_dim = 3
-    key = all_keys[3]
-    # for key in all_keys
-        # total_points = size(ord_matrices[key], 1)
-        #
-        # C_ord_mat[key] = eirene(ord_matrices[key],
-        #                         maxdim=max_B_dim,
-        #                         model="vr",
-        #                         pointlabels=vertices_labels[1:total_points])
-        #
-        # plotbarcode_pjs(C_ord_mat[key])
-        # selected_dim = 1
-        # barcodes = barcode(C_ord_mat[key], dim=selected_dim )
-        # bar = [MyPair(barcodes[k,1], barcodes[k,2]) for k in 1:size(barcodes,1)]
-        # pair_barcodes  = PersistenceBarcodes(bar, selected_dim)
-        #
-        # pl1 = PersistenceLandscape(pair_barcodes)
-        # plot_persistence_landscape(pl1)
-    # end # for
-end
 
 ## ===-
 @testset "intergal of landscapes test" begin
+    ##
     pl0, pl1, pl2, pl3, pl4 = map(
         x -> x |> PersistenceBarcodes |> PersistenceLandscape,
         [
