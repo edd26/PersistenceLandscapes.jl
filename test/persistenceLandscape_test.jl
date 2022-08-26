c011ece777f827c806e1e2b3ac4dd4618691add3
ed19aaf <e.dmitruk@herts.ac.uk>
2022-08-26 10:38:27 +0100

    Exported distances testing

--- test/persistenceLandscape_test.jl
+++ test/persistenceLandscape_test.jl
@@ -409,17 +409,6 @@ end
 end
 ##
 
-## ===-
-@testset "Distances tests" begin
-    @testset "(regular) distance of landscapes" begin
-
-    end
-
-    @testset "max norm distance of landscapes" begin
-
-
-    end
-end
 
 
 
@@ -498,17 +487,6 @@ end
 end
 ##
 
-@testset "computeDiscanceOfLandscapes test" begin
-    pl0, pl1, pl2, pl3, pl4, pl5, pl6, pl7, pl8, pl9 = generate_testing_lanscapes()
-
-    @test computeDiscanceOfLandscapes(pl0, pl0, 0) == 0
-    @test computeDiscanceOfLandscapes(pl1, pl2, 0) == Inf
-    @test computeDiscanceOfLandscapes(pl2, pl3, 0) == Inf
-
-    @test computeDiscanceOfLandscapes(pl0, pl0, 1) == 0
-    @test computeDiscanceOfLandscapes(pl1, pl2, 1) == 3
-    @test computeDiscanceOfLandscapes(pl2, pl3, 1) == 2
-end
 
 ## ===-
 @testset "PersistenceLandscape test on eirene results" begin
