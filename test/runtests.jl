cf1a86a759975791cb97ccb662576c6c2e919ae5
ed19aaf <e.dmitruk@herts.ac.uk>
2022-08-26 10:36:53 +0100

    Added test files; changed config file position

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,17 +2,32 @@ using Test
 using PersistenceLandscapes
 
 # include("test/tests_configuration.jl")
-include("tests_configuration.jl")
+# include("tests_configuration.jl")
 
 ## ===-
 @testset "Test construction of PersistenceBarcode" begin
     include("persistenceBarcode_tests.jl")
 end
 
+## ===-
+@testset "Test contruction of PersistenceLandscapes" begin
+    include("persistenceLandscape_test.jl")
+    include("landscapesConstruction_test.jl")
+end
 
 ## ===-
-@testset "Test contruction of PersistenceLandscape " begin
-    include("persistenceLandscape_test.jl")
+@testset "Test operations on PersistenceLandscapes" begin
+    include("landscapesOperations_test.jl")
+end
+
+## ===-
+@testset "Test distances of PersistenceLandscapes" begin
+    include("landscapesDistances_test.jl")
+end
+
+## ===-
+@testset "Test PersistenceLandscapes plotting" begin
+    include("landscapesPlotting_test.jl")
 end
 
 ## ===-
